077e4ad907fcd0d1d5c76c0bf3ac822ca3a262b3
Aloxan12 <kvakvaaak@mail.ru>
2023-02-09 14:34:07 +0300

    widget text

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        'Hello flutter',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.redAccent,
-          fontSize: 32,
-          fontWeight: FontWeight.w700,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('My first flutter app'),
+          centerTitle: true,
         ),
+        body: Text('This is my homepage', style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+          color: Colors.white
+        ),),
       ),
     ),
   );
