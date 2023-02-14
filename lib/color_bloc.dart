b011c4a5704a0ebf8c357232d96a795cb467993d
Aloxan12 <kvakvaaak@mail.ru>
2023-02-14 15:24:58 +0300

    bloc

--- lib/color_bloc.dart
+++ lib/color_bloc.dart
@@ -1,4 +1,6 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 enum ColorEvent{
@@ -8,4 +10,16 @@ enum ColorEvent{
 
 class ColorBloc{
   Color _color = Colors.red;
+
+  final _inputEventController = StreamController<ColorEvent>();
+  StreamSink<ColorEvent> get inputEventSink => _inputEventController.sink;
+
+  final _outputStateController = StreamController<Color>();
+  Stream<Color> get outputStateStream => _outputStateController.stream;
+
+  void _mapEventToState(Color event){
+    if(event == ColorEvent.evetn_red) _color = Colors.red;
+    else if(event == ColorEvent.evetn_green) _color = Colors.green;
+    else throw Exception('Wrong event types');
+  }
 }
