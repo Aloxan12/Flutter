2377d131dfce928bc1fd205c30388f6e28577361
Aloxan12 <kvakvaaak@mail.ru>
2023-02-16 15:38:59 +0300

    Rick and Morty app

--- lib/feature/domain/usecases/get_all_persons.dart
+++ lib/feature/domain/usecases/get_all_persons.dart
@@ -1,14 +1,25 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_first_progect/core/error/failure.dart';
+import 'package:flutter_first_progect/core/usecases/usecases.dart';
 import 'package:flutter_first_progect/feature/domain/entities/person_entity.dart';
 import 'package:flutter_first_progect/feature/domain/repositories/person_repository.dart';
 
-class GetAllPersons{
+class GetAllPersons extends UseCase<List<PersonEntity>, PagePersonParams>{
   final PersonRepository personRepository;
 
   GetAllPersons(this.personRepository);
 
-  Future<Either<Failure, List<PersonEntity>>> call(int page) async {
-    return await personRepository.getAllPersons(page);
+  Future<Either<Failure, List<PersonEntity>>> call(PagePersonParams params) async {
+    return await personRepository.getAllPersons(params.page);
   }
+}
+
+class PagePersonParams extends Equatable{
+  final int page;
+
+  PagePersonParams({required this.page});
+
+  @override
+  List<Object> get props => [page];
 }
