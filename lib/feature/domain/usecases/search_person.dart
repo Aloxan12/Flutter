81f4975887605e29b0fb9b095e842ee0ef8b742a
Aloxan12 <kvakvaaak@mail.ru>
2023-02-16 14:48:25 +0300

    Rick and Morty app

--- lib/feature/domain/usecases/search_person.dart
+++ lib/feature/domain/usecases/search_person.dart
@@ -3,10 +3,10 @@ import 'package:flutter_first_progect/core/error/failure.dart';
 import 'package:flutter_first_progect/feature/domain/entities/person_entity.dart';
 import 'package:flutter_first_progect/feature/domain/repositories/person_repository.dart';
 
-class GetAllPersons{
+class SearchPerson{
   final PersonRepository personRepository;
 
-  GetAllPersons(this.personRepository);
+  SearchPerson(this.personRepository);
 
   Future<Either<Failure, List<PersonEntity>>> call(int page) async {
     return await personRepository.getAllPersons(page);
