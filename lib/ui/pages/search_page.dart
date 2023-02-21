2147d90fe2a87332e2e08d3a72d2e7c46313d5a0
Aloxan12 <kvakvaaak@mail.ru>
2023-02-21 09:12:29 +0300

    search

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_first_progect/bloc/character_bloc.dart';
+import 'package:flutter_first_progect/data/models/character.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -9,8 +10,18 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  late Character _currentCharacter;
+  List<Results> _currentResults = [];
+  int _currentPage = 1;
+  String _currentSearchStr = '';
+
   @override
   void initState() {
+    if (_currentResults.isEmpty) {
+      context
+          .read<CharacterBloc>()
+          .add(const CharacterEvent.fetch(name: '', page: 1));
+    }
     context
         .read<CharacterBloc>()
         .add(const CharacterEventFetch(name: '', page: 1));
@@ -24,42 +35,61 @@ class _SearchPageState extends State<SearchPage> {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Padding(padding: const EdgeInsets.only(top: 15, bottom: 15, left: 16, right: 16),
-        child: TextField(
-          style: const TextStyle(color: Colors.white),
-          cursorColor: Colors.white,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: const Color.fromRGBO(86, 86, 86, 0.8),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide.none,
+        Padding(
+          padding:
+              const EdgeInsets.only(top: 15, bottom: 15, left: 16, right: 16),
+          child: TextField(
+            style: const TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromRGBO(86, 86, 86, 0.8),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none,
+              ),
+              prefixIcon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              hintText: 'Search name',
+              hintStyle: const TextStyle(color: Colors.white),
             ),
-            prefixIcon: const Icon(Icons.search, color: Colors.white,),
-            hintText: 'Search name',
-            hintStyle: const TextStyle(color: Colors.white),
+            onChanged: (value) {
+              _currentPage = 1;
+              _currentResults = [];
+              _currentSearchStr = value;
+
+              context
+                  .read<CharacterBloc>()
+                  .add(CharacterEvent.fetch(name: value, page: _currentPage));
+            },
           ),
-          onChanged: (value){
-            context.read<CharacterBloc>().add(CharacterEvent.fetch(name: value, page: 1));
-          },
-        ),),
-        state.when(
-            loading: () {
-              return Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    CircularProgressIndicator(strokeWidth: 2),
-                    SizedBox(width: 10),
-                    Text('Loading...'),
-                  ],
-                ),
-              );
-            },
-            loaded: (characterLoaded) {
-              return Text('${characterLoaded.info}');
-            },
-            error: () => const Text('Nothing found...')),
+        ),
+        Expanded(
+          child: state.when(
+              loading: () {
+                return Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      CircularProgressIndicator(strokeWidth: 2),
+                      SizedBox(width: 10),
+                      Text('Loading...'),
+                    ],
+                  ),
+                );
+              },
+              loaded: (characterLoaded) {
+                _currentCharacter = characterLoaded;
+                _currentResults = characterLoaded.results;
+
+                return _currentResults.isNotEmpty
+                    ? Text('$_currentResults')
+                    : const SizedBox();
+              },
+              error: () => const Text('Nothing found...')),
+        ),
       ],
     );
   }
