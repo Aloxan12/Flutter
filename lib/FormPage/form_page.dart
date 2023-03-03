f274fdf6faa3009a2222b05c9294560a34bf03b8
Aloxan12 <kvakvaaak@mail.ru>
2023-03-03 09:56:14 +0300

    Form style

--- lib/FormPage/form_page.dart
+++ lib/FormPage/form_page.dart
@@ -9,6 +9,23 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPage extends State<FormPage> {
+  bool _hidePass = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _storyController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmController = TextEditingController();
+
+  @override
+  void dispose() {
+
+    _nameController.clear();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +36,14 @@ class _FormPage extends State<FormPage> {
         centerTitle: true,
       ),
       body: Form(
+        key: _formKey,
         child: ListView(
           padding: const EdgeInsets.all(16),
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
+            TextFormField(
+              controller: _nameController,
+              validator: (val)=> val!.isEmpty ? 'Name is required' : null,
+              decoration: const InputDecoration(
                 labelText: 'Full Name*',
                 hintText: 'What do people call you?',
                 prefixIcon: Icon(Icons.person),
@@ -37,11 +57,15 @@ class _FormPage extends State<FormPage> {
                 focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20)),
                     borderSide: BorderSide(color: Colors.blue, width: 2)),
+                errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.red, width: 2)),
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(
+              controller: _phoneController,
+              decoration: const InputDecoration(
                 labelText: 'Phone number *',
                 hintText: 'what is your number',
                 helperText: 'Phone format (xxx)xxx-xxx',
@@ -56,10 +80,14 @@ class _FormPage extends State<FormPage> {
                 focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20)),
                     borderSide: BorderSide(color: Colors.blue, width: 2)),
+                errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.red, width: 2)),
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextFormField(
+              controller: _emailController,
               decoration: InputDecoration(
                   labelText: 'Email address',
                   hintText: 'Enter a email address',
@@ -67,6 +95,7 @@ class _FormPage extends State<FormPage> {
             ),
             SizedBox(height: 10),
             TextFormField(
+              controller: _storyController,
               decoration: InputDecoration(
                 labelText: 'Life story',
                 hintText: 'Tell us about yourself',
@@ -75,38 +104,52 @@ class _FormPage extends State<FormPage> {
               ),
               maxLines: 3,
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextFormField(
+              controller: _passwordController,
+              maxLength: 8,
               decoration: InputDecoration(
                 labelText: 'Password *',
                 hintText: 'Enter the password',
-                suffixIcon: Icon(Icons.visibility),
+                suffixIcon: Icon(this._hidePass ? Icons.visibility_off : Icons.visibility,),
                 icon: Icon(Icons.security),
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(
+              controller: _confirmController,
+              maxLength: 8,
+              decoration: const InputDecoration(
                 labelText: 'Confirm password *',
                 hintText: 'Enter the confirm password',
                 suffixIcon: Icon(Icons.visibility),
                 icon: Icon(Icons.border_color),
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextButton(
-              onPressed: () {},
-              child: Text(
+              onPressed: _submitForm,
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
+              ),
+              child: const Text(
                 'Submit form',
                 style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
               ),
             )
           ],
         ),
       ),
     );
+  }
+
+  void _submitForm(){
+    if(_formKey.currentState!.validate()){
+      print('name ${_nameController.text}');
+      print('phone ${_phoneController.text}');
+      print('email ${_emailController.text}');
+      print('story ${_storyController.text}');
+      print('pass ${_passwordController.text}');
+    }
   }
 }
