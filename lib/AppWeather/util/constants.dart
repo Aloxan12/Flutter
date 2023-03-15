121095ec99109e250ceadc9b2a8d05f9432918d8
Aloxan12 <kvakvaaak@mail.ru>
2023-03-15 15:22:09 +0300

    Form valid

---
+++ lib/AppWeather/util/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const String WEATHER_APP_ID = '9f7d2979e2ccc5c3ee8b70532772abea';
+  static const String WEATHER_BASE_SCHEME = 'https://';
+  static const String WEATHER_BASE_URL_DOMAIN = 'samples.openweathermap.org';
+  static const String WEATHER_FORECAST_PATH = 'data/2.5/forecast/daily';
+  static const String WEATHER_IMAGES_PATH = '/img/w/';
+  static const String WEATHER_IMAGES_URL = WEATHER_BASE_SCHEME + WEATHER_BASE_URL_DOMAIN + WEATHER_IMAGES_PATH;
+}
